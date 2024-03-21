39bcf6bde58fdb705e56080faf8fec703bb282ad
Carl Mes <carl.mes@gmail.com>
2024-03-21 15:30:52 -0600

    Create wipenodes.sh

---
+++ wipenodes.sh
@@ -0,0 +1,1 @@
+for node in $(oc get nodes --show-kind --no-headers|awk '/node/{print $1}'); do oc debug $node -- chroot /host wipefs -af /dev/sda; oc debug $node -- chroot /host wipefs -af /dev/nvme0n1; oc debug $node -- chroot /host wipefs -af /dev/nvme1n1; oc debug $node -- chroot /host systemctl poweroff; done
