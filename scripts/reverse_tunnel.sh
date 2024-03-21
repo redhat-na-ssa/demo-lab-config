8879d1d8d8ebe65ceb51b1658672420d0cc6f8e6
Cory Latschkowski <koree@redhat.com>
2024-03-21 17:28:14 -0500

    update: tunnel

--- scripts/reverse_tunnel.sh
+++ scripts/reverse_tunnel.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-kludge_tunnel(){
-  PUBLIC_IP=18.116.38.68
-  OCP_API_IP=172.29.172.200
-  OCP_APP_IP=172.29.172.201
-  OCP_DNS_NAME=cluster1.wf.edgelab.online
+PUBLIC_IP=18.116.38.68
+LAB_IP=143.166.117.0/24
+OCP_API_IP=172.29.172.200
+OCP_APP_IP=172.29.172.201
+OCP_DNS_NAME=cluster1.wf.edgelab.online
 
+kludge_tunnel(){
   echo "Setup your dns to resolve:
 
   *.apps.${OCP_DNS_NAME}  ${PUBLIC_IP}
   api.${OCP_DNS_NAME}     ${PUBLIC_IP}
   "
 
-  ssh -N -p 80 \
+  ssh -N -p 443 \
     root@"${PUBLIC_IP}" \
     -R 0.0.0.0:80:"${OCP_APP_IP}":80 \
     -R 0.0.0.0:443:"${OCP_APP_IP}":443 \
@@ -28,10 +29,9 @@ kludge_test(){
 
 kludge_iptables(){
   iptables -t nat \
-    -I PREROUTING \
-    --src 143.166.117.0/24 \
-    -p tcp --dport 80 \
-    -j REDIRECT --to-ports 22
+    -I PREROUTING -s "${LAB_IP}" -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 22
+  iptables -t nat \
+    -I PREROUTING -s "${LAB_IP}" -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 22
 }
 
 kludge_tunnel
