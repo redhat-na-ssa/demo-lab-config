3631995f8e7d988d642518b9f43c179c21ab143c
Cory Latschkowski <koree@redhat.com>
2024-04-26 15:25:26 -0500

    update: chatbot

--- scripts/chat_bot.sh
+++ scripts/chat_bot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 kludgebot_get_first_model(){
-  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/models'
-  curl -s "${ENDPOINT}" \
+  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com'
+  curl -s "${ENDPOINT}/api/models" \
     -H 'Content-Type: application/json' \
     -d $'{"key": ""}' | jq .[0] | sed 's/ //g' | tr -d '\n'
 }
 
 ask_api(){
-  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/chat'
+  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com'
   MODEL=$(kludgebot_get_first_model)
   PROMPT=${1:-How long does it take to install OpenShift}
 
@@ -21,7 +21,7 @@ ask_api(){
   "
   
   set -x
-  curl -s "${ENDPOINT}" \
+  curl -s "${ENDPOINT}/api/chat" \
     -H 'Content-Type: application/json' \
     -d $'{"model":'"${MODEL}"',"messages":[{"role":"user","content":"'"${PROMPT}"'"}]}'
   set +x
