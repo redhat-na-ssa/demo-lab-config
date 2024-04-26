0cb4b8639f27d1de7e318f86c4ed52f06de5f72f
Cory Latschkowski <koree@redhat.com>
2024-04-26 14:52:39 -0500

    update: chatbot

--- scripts/chat_bot.sh
+++ scripts/chat_bot.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 ask_api(){
-  PROMPT=${1:-What is Red Hat}
   ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/chat'
   # MODEL='{"id":"NousResearch/Llama-2-7b-chat-hf","name":"NousResearch/Llama-2-7b-chat-hf"}'
   MODEL=$(get_first_model)
+  PROMPT=${1:-How long does it take to install OpenShift}
 
-    curl "${ENDPOINT}" \
+
+  curl -s "${ENDPOINT}" \
     -H 'Content-Type: application/json' \
     -d $'{"model":'"${MODEL}"',"messages":[{"role":"user","content":"'"${PROMPT}"'"}]}'
 }
 
 get_first_model(){
   ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/models'
-    curl "${ENDPOINT}" \
+  curl -s "${ENDPOINT}" \
     -H 'Content-Type: application/json' \
     -d $'{"key": ""}' | jq .[0] | sed 's/ //g' | tr -d '\n'
 }
