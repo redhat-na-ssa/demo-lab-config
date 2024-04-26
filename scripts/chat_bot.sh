0c8e51d82dc84699ecbfacdc52f207391856af72
Cory Latschkowski <koree@redhat.com>
2024-04-26 15:03:33 -0500

    update: chatbot

--- scripts/chat_bot.sh
+++ scripts/chat_bot.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+kludgebot_get_first_model(){
+  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/models'
+  curl -s "${ENDPOINT}" \
+    -H 'Content-Type: application/json' \
+    -d $'{"key": ""}' | jq .[0] | sed 's/ //g' | tr -d '\n'
+}
+
 ask_api(){
   ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/chat'
-  # MODEL='{"id":"NousResearch/Llama-2-7b-chat-hf","name":"NousResearch/Llama-2-7b-chat-hf"}'
-  MODEL=$(get_first_model)
+  MODEL=$(kludgebot_get_first_model)
   PROMPT=${1:-How long does it take to install OpenShift}
 
-  echo "PROMPT: ${PROMPT}"
+  echo "
+  ENDPOINT: ${ENDPOINT}
+  MODEL: ${MODEL}
+  PROMPT: ${PROMPT}
   
-  echo "DEBUG: ON"
+  DEBUG: ON
+  "
+  
   set -x
   curl -s "${ENDPOINT}" \
     -H 'Content-Type: application/json' \
     -d $'{"model":'"${MODEL}"',"messages":[{"role":"user","content":"'"${PROMPT}"'"}]}'
   set +x
-}
-
-get_first_model(){
-  ENDPOINT='https://test-llama2-autoscale-runai-llm-training1.apps.cluster1.sandbox284.opentlc.com/api/models'
-  curl -s "${ENDPOINT}" \
-    -H 'Content-Type: application/json' \
-    -d $'{"key": ""}' | jq .[0] | sed 's/ //g' | tr -d '\n'
 }
