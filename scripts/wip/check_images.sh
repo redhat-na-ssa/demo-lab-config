644f01006ff56c383a67c9e8e290812e09393450
Cory Latschkowski <koree@redhat.com>
2024-05-01 19:22:29 -0500

    add: script

--- scripts/wip/check_images.sh
+++ scripts/wip/check_images.sh
@@ -2,13 +2,22 @@
 # shellcheck disable=SC2002,SC2013
 
 check_images(){
-  for image in $(cat docs/images/imageset-config-all-images.txt | sed '0,/Images/d')
+  for image in $(cat docs/images/imageset-config-all-images.txt | sed '0,/Images/d; /quay.io/d; /registry.redhat.io/d; /registry.connect.redhat.com/d')
   do
     printf '%s ' "${image}"
-    if skopeo inspect docker://"${image}" > /dev/null 2>&1; then
+    if skopeo inspect --raw docker://"${image}" > /dev/null 2>&1; then
       echo "[OK]"
     else
       echo "[ERROR]"
     fi
+  done
+}
+
+check_images_from_map(){
+  for image in $(cat docs/images/imageset-config-all.map | sed 's/=file.*//')
+  do
+    # podman --log-level debug pull "${image}"
+
+    skopeo inspect --raw docker://"${image}" | jq .config.digest
   done
 }
